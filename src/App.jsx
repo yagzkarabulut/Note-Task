c420fbd86cfc45b04ef9ba1765d3950315e22b0a
Hüseyin <hsyn51671@gmail.com>
2025-09-19 22:01:13 +0300

    Add note-task project (huseyin-notetask branch)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import EditNote from "./pages/EditNote";
 
 const App = () => {
   return (
-    <div>
-      <h1>Merhaba Dünya</h1>
-    </div>
-  )
-}
+    <BrowserRouter>
+      <div className="min-h-screen bg-indigo-200">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/edit/:id" element={<EditNote />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
